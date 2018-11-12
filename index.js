0fe66d0d2b0b8ea3e7d4abb717f5526acf094d1a
Carlos Martinez <itscarlosisaac@gmail.com>
2018-11-12 10:53:10 -0400

    Using IPC API.

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
 const {setMainMenu} = require('./mainMenu');
+const windows = [];
 
 let mainWindow;
+
+function createBrowserWindow(browserWindowOpts) {
+  let win = new BrowserWindow(Object.assign({
+    width: 400,
+    height: 400,
+  }, browserWindowOpts ));
+
+  windows.push(win);
+  win.loadURL(path.join('file://', __dirname, 'index.html'));
+
+  win.on('close', () => {
+    windows.splice(windows.indexOf(win), 1);
+    sendWindowCount();
+  })
+}
+
+function sendWindowCount(){
+  windows.forEach( win => {
+    win.webContents.send('window-count', {count: windows.length });
+  })
+}
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({
@@ -14,4 +36,6 @@ app.on('ready', () => {
     mainWindow.show();
   })
   setMainMenu(mainWindow);
+  ipcMain.on('create-window', (event, props) => createBrowserWindow(props) );
+  ipcMain.on('get-window-count', sendWindowCount)
 })
--- renderer.js
+++ renderer.js
@@ -1,5 +1,20 @@
-const versionEl = document.querySelector('.version')
+const { ipcRenderer } = require('electron');
+const countEl = document.querySelector('#count');
+const versionEl = document.querySelector('.version');
 versionEl.innerText = process.versions.electron;
 
 console.log(process)
 console.log(process.versions)
+
+document.querySelector('#new-window').addEventListener('click', () => {
+  ipcRenderer.send('create-window', {
+    x: 0,
+    y: 0
+  })
+})
+
+ipcRenderer.on('window-count', (event, props) => {
+  countEl.textContent = props.count;
+});
+
+ipcRenderer.send('get-window-count');
